b7c4be3bc2970f570608737255eaadd497b82719
Kevin Chen <55222995+kevc528@users.noreply.github.com>
2020-01-09 15:05:46 -0600

    exception handling

--- trading.py
+++ trading.py
@@ -11,19 +11,17 @@ from algo import *
 import os
 import time
 import csv
+from pandas_datareader._utils import RemoteDataError
 
-# used for requests
 BASE_URL = 'https://paper-api.alpaca.markets'
 ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
 ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
 HEADERS = {'APCA-API-KEY-ID' : API_KEY, 'APCA-API-SECRET-KEY' : SECRET_KEY}
 
-# return a json of account/portfolio information and settings
 def getAccountInfo():
 	r = requests.get(ACCOUNT_URL, headers = HEADERS)
 	return json.loads(r.content)
 
-# create an order, i.e. buy or sell
 def create_order(symbol, qty, side, type, time_in_force):
 	data = {
 		'symbol' : symbol,
@@ -39,8 +37,6 @@ def create_order(symbol, qty, side, type, time_in_force):
 	except NameError as e:
 		print(e)
 
-# get stocks on the watchlist and portfolio and save them in a set to determine
-# which stocks to watch
 def getWatchList():
 	trending = []
 	fObj = open('watchlist.csv')
@@ -51,29 +47,32 @@ def getWatchList():
 	watchlist = set(watchlist)
 	return watchlist
 
-# updates the portfolio.csv file after transactions
 def updatePortfolio():
 	fObj = open('portfolio.csv', 'w')
 	for key in portfolio:
 		fObj.write(str(key) + ',' + str(portfolio[key]) + '\n')
 	fObj.close()
 
-# cycle function that will repeat (loop), gets stock data and decides what to do
 def cycle():
 	watchlist = getWatchList()
 	for symbol in watchlist:
-		move = decide(symbol)
-		if move == 'sell':
-			create_order(symbol, portfolio[symbol], 'sell', 'market', 'gtc')
-			print('Sold ' + symbol + ' at ' + str(getPrice(symbol)))
-			del portfolio[symbol]
-			updatePortfolio()
-		elif move == 'buy':
-			if float(getAccountInfo()['buying_power']) < 3000:
+		try:
+			move = decide(symbol)
+			if move == 'sell':
+				create_order(symbol, portfolio[symbol], 'sell', 'market', 'gtc')
+				print('Sold ' + symbol + ' at ' + str(getPrice(symbol)))
+				del portfolio[symbol]
+				updatePortfolio()
+			elif move == 'buy':
+				if float(getAccountInfo()['buying_power']) < 3000:
+					continue;
+				create_order(symbol, 3000 // getPrice(symbol), 'buy', 'market', 'gtc')
+				print('Bought ' + symbol + ' at ' + str(getPrice(symbol)))
+				portfolio[symbol] = 3000 // getPrice(symbol)
+				updatePortfolio()
+			else:
 				continue;
-			create_order(symbol, 3000 // getPrice(symbol), 'buy', 'market', 'gtc')
-			print('Bought ' + symbol + ' at ' + str(getPrice(symbol)))
-			portfolio[symbol] = 3000 // getPrice(symbol)
-			updatePortfolio()
-		else:
-			continue;
+		except RemoteDataError as e:
+			print(e)
+		except KeyError as a:
+			print(a)
