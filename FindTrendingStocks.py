ae32b7778f371d5448545ad5aa9db04085f2ceb9
Kevin Chen <55222995+kevc528@users.noreply.github.com>
2020-01-09 15:06:39 -0600

    scraped from new site

--- FindTrendingStocks.py
+++ FindTrendingStocks.py
@@ -17,17 +17,38 @@ import requests
 # 		fObj.write(s + '\n')
 # 	fObj.close()
 
+# def findTrendingStocks():
+
+# 	url = 'https://tradingstockalerts.com/PremiumAlerts/Momentum'
+# 	resp = requests.get(url)
+# 	html = resp.content
+# 	soup = BeautifulSoup(html, 'html.parser')
+# 	table = soup.find('table', {'id' : 'TABLE_1'})
+# 	tr_tags = table.find_all('tr')[1:]
+# 	td_tags = [tag.find_all('td')[1] for tag in tr_tags]
+
+# 	symbols = [tag.text.strip() for tag in td_tags if tag != None and '=' not in tag.text.strip() and '^' not in tag.text.strip()]
+# 	print('run scrape')
+# 	fObj = open('watchlist.csv', 'w')
+# 	for s in symbols:
+# 		fObj.write(s + '\n')
+# 	fObj.close()
+
 def findTrendingStocks():
 
-	url = 'https://tradingstockalerts.com/PremiumAlerts/Momentum'
-	resp = requests.get(url)
-	html = resp.content
-	soup = BeautifulSoup(html, 'html.parser')
-	table = soup.find('table', {'id' : 'TABLE_1'})
-	tr_tags = table.find_all('tr')[1:]
-	td_tags = [tag.find_all('td')[1] for tag in tr_tags]
+	symbols = []
 
-	symbols = [tag.text.strip() for tag in td_tags if tag != None and '=' not in tag.text.strip() and '^' not in tag.text.strip()]
+	def processPage(page):
+		url = page
+		resp = requests.get(url)
+		html = resp.content
+		soup = BeautifulSoup(html, 'html.parser')
+		symbolCol = soup.find_all('td', {'class' : 'last col_symbol'})
+		return [tag.text.strip() for tag in symbolCol]
+
+	symbols += processPage('http://markets.financialcontent.com/stocks/stocks/dashboard/mostactive')
+	symbols += processPage('http://markets.financialcontent.com/stocks/stocks/dashboard/mostactive?CurrentPage=1')
+	symbols += processPage('http://markets.financialcontent.com/stocks/stocks/dashboard/mostactive?CurrentPage=2')
 	print('run scrape')
 	fObj = open('watchlist.csv', 'w')
 	for s in symbols:
