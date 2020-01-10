2d1fdad9b8b66ada17c3c17cbd3ffc1006b6d4f0
Kevin Chen <55222995+kevc528@users.noreply.github.com>
2020-01-09 18:25:22 -0600

    commented code, cleanup

--- TradeBot.py
+++ TradeBot.py
@@ -9,6 +9,7 @@ import csv
 import sqlite3
 import pandas as pd
 
+# prepare the trading bot by getting the portfolio, and getting the watchlist
 initPortfolio()
 findTrendingStocks()
 getWatchList()
@@ -18,13 +19,16 @@ tz_NY = pytz.timezone('America/New_York')
 now = datetime.now(tz_NY)
 start = now.replace(hour=9, minute=30, second=0, microsecond=0)
 
+# checks time for stock market opening and runs the cycle function continuously
 while start <= now and (datetime.now(tz_NY).hour) < 16:
 	cycle()
 
+# stores day's data into a SQL database
 con = sqlite3.connect('BotData.db')
 df = pd.read_csv('DayLog.csv')
 df.to_sql(str(date.today()), con, if_exists = 'append', index = False)
 
+# resets files for the next day
 fObj = open('watchlist.csv', 'w')
 fObj.close()
 
